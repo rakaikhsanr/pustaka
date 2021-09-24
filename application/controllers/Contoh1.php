8eaf906810ca4ebad225f114a2a2879855e3a740
rakaikhsanr <91039796+rakaikhsanr@users.noreply.github.com>
2021-09-24 16:59:16 +0700

    Add files via upload

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class Contoh1 extends CI_Controller {
+
+    public function index()
+    {
+        echo "SELAMAT DATANG MAHASISWA MAHASISWI............SELAMAT DATANG DIKELAS WEB PROGRAMMING II.......";
+    }
+}
